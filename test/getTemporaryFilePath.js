35716892779d1f9e85938f826fa1d2bca89715a0
Andreas Lind <andreaslindpetersen@gmail.com>
2020-07-17 11:17:32 +0200

    Use sinon to do the mocking

--- test/getTemporaryFilePath.js
+++ test/getTemporaryFilePath.js
@@ -10,71 +10,53 @@ try {
     // Not available in older node.js versions
 }
 
-var originalProcessPlatform = process.platform,
-    originalProcessEnv = process.env,
-    originalOs = os,
-    originalOsTmpdir = os && os.tmpdir,
-    originalFsRealpathSync = fs.realpathSync;
-
 describe('getTemporaryFilePath', function () {
     var expect = unexpected.clone().installPlugin(require('unexpected-sinon'));
 
-    // Restore all properties that might be mocked out:
     afterEach(function () {
         delete getTemporaryFilePath.getTempDir.tempDir;
-        process.platform = originalProcessPlatform;
-        process.env = originalProcessEnv;
-        os = originalOs;
-        fs.realpathSync = originalFsRealpathSync;
+        sinon.restore();
     });
 
     describe('#getTempDir()', function () {
         describe('with no os.tmpdir', function () {
             beforeEach(function () {
-                delete os.tmpdir;
+                sinon.stub(os, 'tmpdir').value();
             });
 
             describe('in a win32 environment', function () {
                 beforeEach(function () {
-                    process.platform = 'win32';
+                    sinon.stub(process, 'platform').value('win32');
                 });
                 describe('with a TMP environment variable', function () {
                     beforeEach(function () {
-                        process.env = {TMP: 'C:\\blabla'}
+                        sinon.stub(process, 'env').value({TMP:'C:\\blabla'});
                     });
 
                     it('should return the value of the TMP environment variable if fs.realpathSync says it exists', function () {
-                        fs.realpathSync = sinon.spy(function (path) {
-                            return path;
-                        });
+                        sinon.stub(fs, 'realpathSync').returnsArg(0);
                         expect(getTemporaryFilePath.getTempDir(), 'to equal', 'C:\\blabla');
                     });
 
                     it('should throw if fs.realpathSync says the TMP environment points to a non-existent dir', function () {
-                        fs.realpathSync = function (path) {
-                            throw new Error('ENOENT');
-                        };
+                        sinon.stub(fs, 'realpathSync').throws(new Error('ENOENT'));
                         expect(getTemporaryFilePath.getTempDir, 'to throw', 'ENOENT');
                     });
                 });
 
                 describe('with no TMP environment variable', function () {
                     beforeEach(function () {
-                        process.env = {};
+                        sinon.stub(process, 'env').value({});
                     });
 
                     it('should return c:\\tmp if fs.realpathSync says it exists', function () {
-                        fs.realpathSync = sinon.spy(function (path) {
-                            return path;
-                        });
+                        sinon.stub(fs, 'realpathSync').returnsArg(0);
                         expect(getTemporaryFilePath.getTempDir(), 'to equal', 'c:\\tmp');
                         expect(fs.realpathSync, 'was called once');
                     });
 
                     it('should throw if fs.realpathSync says c:\\tmp does not exist', function () {
-                        fs.realpathSync = sinon.spy(function (path) {
-                            throw new Error('ENOENT');
-                        });
+                        sinon.stub(fs, 'realpathSync').throws(new Error('ENOENT'));
                         expect(getTemporaryFilePath.getTempDir, 'to throw', 'ENOENT');
                         expect(fs.realpathSync, 'was called once');
                     });
